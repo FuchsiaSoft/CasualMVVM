1f43b5b2448ca2d15da4dde76d048e45f8240c81
Chris Wilson <FuchsiaSoft@users.noreply.github.com>
2016-03-03 13:13:48 +0000

    Window mediation done

---
+++ CasualMVVM/WindowMediation/IWindowService.cs
@@ -0,0 +1,20 @@
+﻿namespace FuchsiaSoft.CasualMVVM.WindowMediation
+{
+    /// <summary>
+    /// Provides the interface for opening new Windows
+    /// ala MVVM.  Windows will all be opened blank, and
+    /// their content property will be set as a xaml page
+    /// </summary>
+    public interface IWindowService
+    {
+        /// <summary>
+        /// Opens a blank window and sets the content
+        /// for it to the requisite xaml Page
+        /// (found from DataTemplate set in App.xaml)
+        /// </summary>
+        /// <param name="ViewModel">The ViewModel to open
+        /// the window for</param>
+        /// <param name="type">The window type to open</param>
+        void ShowWindow(object ViewModel, WindowType type);
+    }
+}
