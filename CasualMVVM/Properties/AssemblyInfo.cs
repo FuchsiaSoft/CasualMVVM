3ec6a4f3069cc04ebcc2b76761b82d9ffd8f5e8e
Chris Wilson <FuchsiaSoft@users.noreply.github.com>
2016-03-03 09:09:50 +0000

    initial commit

---
+++ CasualMVVM/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CasualMVVM")]
+[assembly: AssemblyDescription("A simple, every day library to support MVVM with WPF for common applications")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("FuchsiaSoft")]
+[assembly: AssemblyProduct("CasualMVVM")]
+[assembly: AssemblyCopyright("Copyright ©  2016 Chris Wilson")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dedd22fa-ed73-4622-a8ca-d98b53dc9dab")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
